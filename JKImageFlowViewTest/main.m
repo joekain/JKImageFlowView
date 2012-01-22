50fc01948f00df455928b8133f6df3d8f93cbb12
Joe Kain <jkain.dev@gmail.com>
2012-01-21 22:13:44 -0800

    [JKImageFlowView] Initial project
    JKImageFlowView is a Framework that will include the JKImageFlowView
    class and associated data.
    
    JKImageFlowViewTest is a simple test application that will be used to
    verify the JKImageFlowView class.

---
+++ JKImageFlowViewTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  JKImageFlowViewTest
+//
+//  Created by Joseph Kain on 1/21/12.
+//  Copyright (c) 2012 Joseph Kain. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
